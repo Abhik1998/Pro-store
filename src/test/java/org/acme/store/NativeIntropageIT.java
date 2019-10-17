3d9c41db77f0684788a4618e429cb31ed95f4753
Abhik1998 <abhik1998chakraborty@gmail.com>
2019-10-17 10:28:38 +0530

    Added my new project

---
+++ src/test/java/org/acme/store/NativeIntropageIT.java
@@ -0,0 +1,9 @@
+package org.acme.store;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeIntropageIT extends IntropageTest {
+
+    // Execute the same tests but in native mode.
+}
